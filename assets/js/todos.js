374c6b28694e8a8ac8e4698cb9e401293bd6234e
nicolehli <nicolehcli@gmail.com>
2017-12-10 13:34:12 -0800

    Added item strikethrough and unstrikethrough upon clicking event

--- assets/js/todos.js
+++ assets/js/todos.js
@@ -1,1 +1,12 @@
-// alert("CONNECTED");
+// Check Off Specific Todos by clicking
+$("li").click(function() {
+  // TEST alert("You've clicked an LI item");
+  // Version 1: $(this).css("color", "gray");
+  // Version 1: $(this).css("text-decoratino", "line-through");
+  // Version 2: $(this).css({
+  //   color: "gray";
+  //   textDecoration: "line-through"; // NOTE: js object names with hyphen is not allowed
+  // });
+  // Version 3: Moved to CSS in a "completed" class
+  $(this).toggleClass("completed");
+});
